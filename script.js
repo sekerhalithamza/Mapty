385d8c43e9261a8999001c2b8c7508d723d59248
Halit Hamza Şeker <shalithamza@gmail.com>
2023-08-26 22:32:35 +0300

    Using Leaflet library

--- script.js
+++ script.js
@@ -16,6 +16,17 @@ if (navigator.geolocation)
     (position) => {
       const { latitude, longitude } = position.coords;
       const link = `https://www.google.com/maps/@${latitude},${longitude}`;
+      const map = L.map("map").setView([latitude, longitude], 15);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
     },
     () => {
       alert("Could not get your location.");
